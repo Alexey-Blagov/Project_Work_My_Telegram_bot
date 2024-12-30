ff64c65718c5044018eae9423827185c719cc864
Alexey-Blagov <alexeyblmi8@gmail.com>
2024-12-30 12:04:56 +0300

    Добавьте файлы проекта.

---
+++ Project_Work_My_Telegram_bot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Project_Work_My_Telegram_bot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Project_Work_My_Telegram_bot/UpdateHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Work_My_Telegram_bot
+{
+    public class UpdateHandler
+    {
+    }
+}
