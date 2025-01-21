21689d06de54dae012c87ac811a2ab483bffcfc3
Alexey-Blagov <alexeyblmi8@gmail.com>
2025-01-21 21:56:43 +0300

    +

--- Project_Work_My_Telegram_bot/DataBaseHandler.cs
+++ Project_Work_My_Telegram_bot/DataBaseHandler.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot.Requests;
 
 namespace Project_Work_My_Telegram_bot
 {
-    internal class DataBaseHandler
+    public class DataBaseHandler
     {
+        public void GetUser()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+
+            };
+        }
+
         // получение роли юзера
         //public static async Task <User> GetUserRole(long TgId)
         //{
---
+++ Project_Work_My_Telegram_bot/Migrations/20250121184227_InitCreate.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Project_Work_My_Telegram_bot.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarDrives",
+                columns: table => new
+                {
+                    CarId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CarName = table.Column<string>(type: "text", nullable: true),
+                    CarNumber = table.Column<string>(type: "text", nullable: false),
+                    GasСonsum = table.Column<double>(type: "double precision", nullable: false),
+                    TypeFuel = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarDrives", x => x.CarId);
+                    table.UniqueConstraint("AK_CarDrives_CarNumber", x => x.CarNumber);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    IdTg = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TgUserName = table.Column<string>(type: "text", nullable: false),
+                    UserRol = table.Column<string>(type: "text", nullable: false),
+                    UserName = table.Column<string>(type: "text", nullable: true),
+                    JobTitlel = table.Column<string>(type: "text", nullable: true),
+                    CarId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.IdTg);
+                    table.ForeignKey(
+                        name: "FK_Users_CarDrives_CarId",
+                        column: x => x.CarId,
+                        principalTable: "CarDrives",
+                        principalColumn: "CarId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ObjectPaths",
+                columns: table => new
+                {
+                    IdPath = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ObjectName = table.Column<string>(type: "text", nullable: false),
+                    PathLengh = table.Column<float>(type: "real", nullable: false),
+                    DatePath = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: true),
+                    CarId = table.Column<int>(type: "integer", nullable: true),
+                    CarDriveCarId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ObjectPaths", x => x.IdPath);
+                    table.ForeignKey(
+                        name: "FK_ObjectPaths_CarDrives_CarDriveCarId",
+                        column: x => x.CarDriveCarId,
+                        principalTable: "CarDrives",
+                        principalColumn: "CarId");
+                    table.ForeignKey(
+                        name: "FK_ObjectPaths_Users_CarId",
+                        column: x => x.CarId,
+                        principalTable: "Users",
+                        principalColumn: "IdTg");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OtherExpenses",
+                columns: table => new
+                {
+                    ExpId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    NameExpense = table.Column<string>(type: "text", nullable: false),
+                    Coast = table.Column<decimal>(type: "numeric", nullable: false),
+                    DateTimeExp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OtherExpenses", x => x.ExpId);
+                    table.ForeignKey(
+                        name: "FK_OtherExpenses_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "IdTg");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ObjectPaths_CarDriveCarId",
+                table: "ObjectPaths",
+                column: "CarDriveCarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ObjectPaths_CarId",
+                table: "ObjectPaths",
+                column: "CarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OtherExpenses_UserId",
+                table: "OtherExpenses",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CarId",
+                table: "Users",
+                column: "CarId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ObjectPaths");
+
+            migrationBuilder.DropTable(
+                name: "OtherExpenses");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "CarDrives");
+        }
+    }
+}
